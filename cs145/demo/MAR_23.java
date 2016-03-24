eff62a713dcf3746795d34ffdf72db6c05a11737
Richard NMHU <rmedina@nmhu.edu>
2016-03-24 07:19:34 -0600

    edits

--- cs145/demo/MAR_23.java
+++ cs145/demo/MAR_23.java
@@ -3,12 +3,15 @@
 
 class MAR_23 {
 	
+// MAIN EXECUTION
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
 		int a = 89;
 		int b = 900;
-		int c = 89 + 900;
+		
+		double floater = 8.9;
+		
+		int c = a + b;
 		System.out.println(c);
 	
 		int count = 1;
@@ -16,8 +19,7 @@ class MAR_23 {
 			if (count % 2 == 0) {
 				System.out.println("Count" + count);
 			}
-			
 			count = count + 1;
 		}
-	}
+	} // DONE!!!!!
 }
